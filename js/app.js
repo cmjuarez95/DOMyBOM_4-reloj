a439a3dc948b2f9d06ea4240885c7ec7435526bd
Mariano Juarez <cm.juarez.95@gmail.com>
2025-07-20 19:33:07 -0300

    ejercicio 4 realizado

--- js/app.js
+++ js/app.js
@@ -5,8 +5,30 @@ const opcionesFecha={
     year: "numeric", //'numeric' (ej. "2023"), '2-digit' (ej. "23")
     month: "long", // 'numeric' (ej. "1"), '2-digit' (ej. "01"), 'long' (ej. "enero"), 'short' (ej. "ene"), 'narrow' (ej. "E")
     day: "numeric", //'numeric' (ej. "3"), '2-digit' (ej. "03")
+}
+
+const opcionesHora={
+    hora: "numeric",
+    minutos:"numeric",
+    segundos:"numeric",
+    hour12: false // Esto fuerza el formato de 24 horas
 
 }
 
+function actualizarReloj() {
+    const horaActual=new Date()
+    const horaFormateada=horaActual.toLocaleTimeString(undefined,opcionesHora)
+    hora.textContent=horaFormateada
+}
+
+const fecha = document.querySelector("#fecha")
+let hora = document.getElementById("hora")
+
+actualizarReloj()
 const fechaFormateada= fechaActual.toLocaleDateString(undefined, opcionesFecha)
-console.log(fechaFormateada)
+fecha.textContent=fechaFormateada
+setInterval(actualizarReloj,1000)
+
+
+
+
